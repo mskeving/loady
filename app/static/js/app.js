1df37b3395aa752aaf86973973f877b2a8d37acc
Phil Rha <philrha@gmail.com>
2016-12-09 09:42:55 -0800

    add favicon

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import { PageContainer } from './components/PageContainer';
 
-document.addEventListener("DOMContentLoaded", () =>
+document.addEventListener('DOMContentLoaded', () =>
 	ReactDOM.render(
 	  <PageContainer />,
-	  document.getElementById('content')
+	  document.getElementById('react-root')
 	)
 );
