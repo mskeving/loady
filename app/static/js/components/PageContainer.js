f508383e49b14fb127ef0b9cc47c5def8525bbc9
Phil Rha <philrha@gmail.com>
2016-12-10 13:01:09 -0800

    add lint rules

--- app/static/js/components/PageContainer.js
+++ app/static/js/components/PageContainer.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 export class CustomComponent extends Component {
-	render() {
-		const { message } = this.props;
-		return <div>{ message }</div>;
-	}
+  static propTypes = {
+    message: PropTypes.string.isRequired,
+  }
+
+  render() {
+    const { message } = this.props;
+    return <div>{ message }</div>;
+  }
 }
 
 export class PageContainer extends Component {
-	render() {
-		return (
-			<div>
-				<CustomComponent message="Your new app" />
-			</div>
-		)
-	}
+  render() {
+    return (
+      <CustomComponent message="Your new app" />
+    );
+  }
 }
