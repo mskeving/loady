dfe4a97f5879d67be88fdb6aea594e9afdaa6ff1
Phil Rha <philrha@gmail.com>
2016-12-14 13:18:38 -0800

    add scss support

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { PageContainer } from './components/PageContainer';
 
+require('scss/main.scss');
+
 document.addEventListener('DOMContentLoaded', () => {
   const reactRoot = document.getElementById('react-root');
   ReactDOM.render(<PageContainer />, reactRoot);
--- app/static/js/webpack.config.js
+++ app/static/js/webpack.config.js
@@ -1,23 +1,35 @@
 var webpack = require('webpack');
+var path = require('path');
+var staticRoot = path.resolve(__dirname, '..');
 
 module.exports = {
-    entry: {
-        app: "./app.js",
+  entry: {
+    app: "./app.js",
+  },
+  devtool: 'source-map',
+  output: {
+    path: __dirname,
+    publicPath: "http://localhost:8080/assets/",
+    filename: "build/[name].bundle.js",
+  },
+  resolve: {
+    alias: {
+      'scss': path.resolve(staticRoot, 'scss'),
     },
-    devtool: 'source-map',
-    output: {
-        path: __dirname,
-        publicPath: "http://localhost:8080/assets/",
-        filename: "build/[name].bundle.js"
-    },
-    resolve: {
-      extensions: ['', '.js']
-    },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            loaders: ['react-hot', 'babel', 'eslint'],
-            exclude: /node_modules/
-        }]
-    }
+    extensions: ['', '.js', '.jsx'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['react-hot', 'babel', 'eslint'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+        exclude: /node_modules/,
+      }
+    ],
+  }
 };
